5f7bb5aee1c13f1df77af08a8a0281d92846a47f
jerlin-05 <jerlinmathew2005@gmail.com>
2025-09-27 00:23:16 +0530

    Update API baseURL to deployed backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://mern-a1.onrender.com/api"   
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
